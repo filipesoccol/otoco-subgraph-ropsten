755698442bf50b15c8011348077989a84975a575
Filipe Soccol <filipe.soccol@gmail.com>
2021-10-14 18:17:49 -0300

    Change target DAO contract

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
 export const MASTER_DELAWARE = '0x861995a262516a98c1face4c63bee22fbc6251c6'
 export const MASTER_WYOMING = '0x8f332a747e4907fa51921f4e025cb1ae6fbbe9b2'
-export const MASTER_DAO = '0x40729ece9d0390163cebe4bdf93b7600df574673'
+export const MASTER_DAO = '0xaca24291143efd88b3ec594a9b5247c762d2aecb'
 export const MASTER_REGISTRY = '0x8f332a747e4907fa51921f4e025cb1ae6fbbe9b2'
